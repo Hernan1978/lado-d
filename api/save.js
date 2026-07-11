9389373596034dab93fe751ed21485144bd6f767
Hernan1978 <hernan.medina1978@hotmail.com>
2026-07-11 15:49:07 -0300

    Update save.js

--- api/save.js
+++ api/save.js
@@ -7,17 +7,21 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  if (req.method !== 'POST') {
+    return res.status(405).json({ status: 'error', message: 'Method not allowed' });
+  }
+
   const SHEET_API = 'https://script.google.com/macros/s/AKfycbwO7TVauAJNFXsK8mAQrePvbGEeZIMjvOX81HP2LGMmO9i1TzKmyswVdhpzK3USDwk/exec';
 
   try {
-    const body = req.body;
+    const data = req.body;
     const params = new URLSearchParams();
     params.append('action', 'save');
-    params.append('data', JSON.stringify(body));
+    params.append('data', JSON.stringify(data));
 
     const response = await fetch(`${SHEET_API}?${params.toString()}`);
-    const data = await response.json();
-    res.status(200).json(data);
+    const result = await response.json();
+    res.status(200).json(result);
   } catch(err) {
     res.status(500).json({ status: 'error', message: err.message });
   }
